588beef9c222accc21443179fa1409e00fbaa550
Jimmy Klein <jimmy.klein4@gmail.com>
2016-12-06 16:04:34 -0500

    Updated to pertain to my stuff

--- app/src/main/java/edu/temple/tuf21842/bitcoindashboard/MainActivity.java
+++ app/src/main/java/edu/temple/tuf21842/bitcoindashboard/MainActivity.java
@@ -3,7 +3,6 @@ package edu.temple.tuf21842.bitcoindashboard;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
@@ -13,7 +12,7 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-    private String[] mPlanetTitles;
+    private String[] mTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
 
@@ -22,44 +21,45 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mTitles = getResources().getStringArray(R.array.planets_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mPlanetTitles));
+        mDrawerList.setAdapter(new ArrayAdapter<>(this,
+                R.layout.drawer_list_item, mTitles));
         mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //selectItem(position);
+                selectItem(i);
             }
         });
     }
-/*
-    private void selectItem(int position) {
-        // Create a new fragment and specify the planet to show based on position
-        Fragment fragment = new PlanetFragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);
 
-        // Insert the fragment by replacing any existing fragment
+    private void selectItem(int position) {
+        Fragment fragment;
+        switch(position) {
+            case 0:
+                fragment = new PriceFragment();
+                break;
+            case 1:
+                fragment = new BlockInfoFragment();
+                break;
+            case 2:
+                fragment = new BalanceFragment();
+                break;
+            default:
+                fragment = null;
+                break;
+        }
+
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, fragment)
                 .commit();
 
-        // Highlight the selected item, update the title, and close the drawer
         mDrawerList.setItemChecked(position, true);
-        setTitle(mPlanetTitles[position]);
+        setTitle(mTitles[position]);
         mDrawerLayout.closeDrawer(mDrawerList);
     }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
-    */
 
 }
